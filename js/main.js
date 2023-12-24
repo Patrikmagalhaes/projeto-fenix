f9f3990545a7c8d576d29c788975d6183fb63251
Patrikmagahaes <patrikmagalhaes@icloud.com>
2023-12-24 17:16:35 -0300

    criando scrollBar e atalho para o topo da pagina

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-document.addEventListener("DOMContentLoaded", function ()) {
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btnSubir = document.getElementById('btn-subir')
+    const cabecalho = document.getElementById('inicio')
+    const seta = document.getElementById('seta')
+
+    let direcao = 0
     window.addEventListener("scroll", function () {
 
+        var quantidadeRolada = document.body.scrollTop || document.documentElement.scrollTop;
+        var quantidadeRolavel = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        var porcentagemDoscroll = (quantidadeRolada / quantidadeRolavel) * 100
+        document.getElementById("scrollBar").style.width = porcentagemDoscroll + "%";
+
+        function ScrollTop() {
+            btnSubir.addEventListener('click', function () {
+                cabecalho.scrollIntoView()
+            })
+        }
+        if (scrollY < direcao) {
+            ScrollTop()
+            seta.classList.remove('off')
+        }
+        else {
+            seta.classList.add('off')
+        }
+        direcao = scrollY
     })
-}
+})
