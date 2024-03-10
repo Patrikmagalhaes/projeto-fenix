066c992f1af3f4a8328cfc5e87a8e7f624f58f0a
cassia figueiredo <figueiredo.cassia05@gmail.com>
2024-03-10 19:50:44 -0300

    ajustando layout

--- js/emailConfig.js
+++
@@ -1,8 +0,0 @@
-const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'lauretta.klocko@ethereal.email',
-        pass: 'kmFx73wyCWMRuHpv9T'
-    }
-});
